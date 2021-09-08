fe7468e219dcbb2c0d24418712a14a5a9d74241e
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-09-08 04:23:47 -0300

    feat : migration tabel palavra

---
+++ src/database/migrations/20210905150656-createTable-Palavra.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable("Palavra", {
+        id_palavra: {
+          type: Sequelize.DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        nome: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+        },
+        significado: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+        },
+      });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("Palavra");
+  },
+};
