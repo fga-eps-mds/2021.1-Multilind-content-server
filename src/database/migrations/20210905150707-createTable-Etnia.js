3601214e8e0b31478fff54d622de2a74505debae
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-09-08 04:24:00 -0300

    feat : migration tabel etnia

---
+++ src/database/migrations/20210905150707-createTable-Etnia.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable("Etnia", {
+        id_etnia: {
+          type: Sequelize.DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        nome: {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false,
+        },
+      });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("Etnia");
+  },
+};
