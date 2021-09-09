458f994d9ee3d8030f47d6586d0da74472b47e49
CarlosZoft <carlosoftwar@gmail.com>
2021-09-08 23:51:29 -0300

    feat: defaut exports
    Co-authored-by: Herick <herick.lima77@gmail.com>
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/controllers/Etnia/index.js
+++ src/controllers/Etnia/index.js
@@ -1,1 +1,5 @@
-export * from './createEtnia';
+export { create } from "./createEtnia";
+export { getOne } from "./getOneEtnia";
+export { getAll } from "./getAllEtnia";
+export { update } from "./updateEtnia";
+export { deleteOne } from "./deleteEtnia";
