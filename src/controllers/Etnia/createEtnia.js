c32f68e1a0b6d0d7ba2c6d2268e5a92e78263b52
CarlosZoft <carlosoftwar@gmail.com>
2021-09-08 23:47:03 -0300

    feat: create Etnia controller
    Co-authored-by: Herick <herick.lima77@gmail.com>
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/controllers/Etnia/createEtnia.js
+++ src/controllers/Etnia/createEtnia.js
@@ -1,23 +1,19 @@
-const Etnia = require('../../models/Etnia');
-const { HttpException } = require('../../error/HttpException');
+import Etnia from "../../models/Etnia";
+import { HttpException } from "../../error/HttpException";
 
 exports.create = async (request, response) => {
   const { body } = request;
   const { nome } = body;
-
-  if(!nome){
+  if (!nome) {
     throw new HttpException(400, "Nome inválido - Etnia");
   }
-  try{
-    const nameAlreadyExists = await Etnia.searchByName(nome);
-    if(nameAlreadyExists){
-      throw new HttpException(400, "Nome já existente - Etnia");
-    } 
-    await Etnia.create(body);
 
-    response.send(body);
+  const nameAlreadyExists = await Etnia.searchByName(nome);
+  if (nameAlreadyExists) {
+    throw new HttpException(400, "Nome já existente - Etnia");
   }
-  catch(err){
-    throw err;
-  }
-}
+
+  await Etnia.create(body);
+
+  response.send(body);
+};
