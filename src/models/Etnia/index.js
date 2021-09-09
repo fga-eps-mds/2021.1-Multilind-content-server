caa63ac1a513522d7931556ea5d4ac8f75b2da06
CarlosZoft <carlosoftwar@gmail.com>
2021-09-09 00:36:22 -0300

    refactor: improving visual code
    Co-authored-by: Herick <herick.lima77@gmail.com>
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/models/Etnia/index.js
+++ src/models/Etnia/index.js
@@ -1,61 +1,33 @@
 import EtniaModel from "./Etnia";
 
-(exports.getAll = async () => {
-  try {
-    return await EtniaModel.findAll({
-      raw: true,
-    });
-  } catch (error) {
-    throw error;
-  }
-}),
-  (exports.create = async (etnia) => {
-    try {
-      return await EtniaModel.create(etnia);
-    } catch (error) {
-      throw error;
-    }
-  }),
-  (exports.searchById = async (id) => {
-    try {
-      return await EtniaModel.findByPk(id);
-    } catch (error) {
-      throw error;
-    }
-  }),
-  (exports.searchByName = async (nome) => {
-    try {
-      return await EtniaModel.findOne({
-        where: {
-          nome: nome,
-        },
-      });
-    } catch (error) {
-      throw error;
-    }
-  }),
-  (exports.searchAll = async () => {
-    try {
-      return await EtniaModel.findAll();
-    } catch (error) {
-      throw error;
-    }
+exports.getAll = async () => {
+  return await EtniaModel.findAll({
+    raw: true,
   });
-(exports.delete = async (id) => {
-  try {
-    return await EtniaModel.destroy({
-      where: {
-        id_etnia: id,
-      },
-    });
-  } catch (error) {
-    throw error;
-  }
-}),
-  (exports.editById = async (body, id) => {
-    try {
-      return await EtniaModel.update(body, { where: { id_etnia: id } });
-    } catch (error) {
-      throw error;
-    }
+};
+exports.searchByName = async (nome) => {
+  return await EtniaModel.findOne({
+    where: {
+      nome: nome,
+    },
   });
+};
+exports.create = async (etnia) => {
+  return await EtniaModel.create(etnia);
+};
+exports.searchById = async (id) => {
+  return await EtniaModel.findByPk(id);
+};
+exports.searchAll = async () => {
+  return await EtniaModel.findAll();
+};
+exports.delete = async (id) => {
+  return await EtniaModel.destroy({
+    where: {
+      id_etnia: id,
+    },
+  });
+};
+exports.editById = async (body, id) => {
+  return await EtniaModel.update(body, { where: { id_etnia: id } });
+};
