a5036f44c5ade3cd9317b9668a5fc38564535840
CarlosZoft <carlosoftwar@gmail.com>
2021-09-08 23:52:40 -0300

    refactor: improving organization
    Co-authored-by: Herick <herick.lima77@gmail.com>
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/models/Etnia/index.js
+++ src/models/Etnia/index.js
@@ -1,63 +1,61 @@
-const EtniaModel = require('./Etnia');
+import EtniaModel from "./Etnia";
 
-
-
-exports.getAll = async () => {
-    try{
-        return await EtniaModel.findAll({
-            raw:true,
-        });
-    }
-    catch(error){
-        throw error;
-    }
-},
-exports.create = async (etnia) => {
-    try{
-        return await EtniaModel.create(etnia);
-    }
-    catch(error){
-        throw error;
-    }
-},
-exports.searchById = async (id) => {
-    try{
-        return await EtniaModel.findByPk(id)
-    }
-    catch(error){
-        throw error;
-    }
-},
-exports.searchByName = async (nome) => {
-    try{
-        return await EtniaModel.findOne({
-            where : {
-                nome : nome
-            }
-        })
-    }
-    catch(error){
-        throw error;
-    }
-},
-exports.delete = async (id) => {
+(exports.getAll = async () => {
+  try {
+    return await EtniaModel.findAll({
+      raw: true,
+    });
+  } catch (error) {
+    throw error;
+  }
+}),
+  (exports.create = async (etnia) => {
     try {
-        return await EtniaModel.destroy({
-            where: {
-                id : id
-            }
-        })
+      return await EtniaModel.create(etnia);
+    } catch (error) {
+      throw error;
     }
-    catch(error){
-        throw error;
+  }),
+  (exports.searchById = async (id) => {
+    try {
+      return await EtniaModel.findByPk(id);
+    } catch (error) {
+      throw error;
     }
-},
-exports.editByAdd =  async (body, id) => {
-    try{
-        return await EtniaModel.update(body, {where: {id: id}});
+  }),
+  (exports.searchByName = async (nome) => {
+    try {
+      return await EtniaModel.findOne({
+        where: {
+          nome: nome,
+        },
+      });
+    } catch (error) {
+      throw error;
     }
-    catch(error){
-        throw error;
+  }),
+  (exports.searchAll = async () => {
+    try {
+      return await EtniaModel.findAll();
+    } catch (error) {
+      throw error;
     }
-}
-
+  });
+(exports.delete = async (id) => {
+  try {
+    return await EtniaModel.destroy({
+      where: {
+        id_etnia: id,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+}),
+  (exports.editById = async (body, id) => {
+    try {
+      return await EtniaModel.update(body, { where: { id_etnia: id } });
+    } catch (error) {
+      throw error;
+    }
+  });
