2de1ae16837cbe170219550cf4d283b9e17f942a
CarlosZoft <carlosoftwar@gmail.com>
2021-09-09 23:38:05 -0300

    feat: add model conteudo para relacionamento Co-authored-by: Herick <herick.lima77@gmail.com> Co-authored-by: Helder <180121847@aluno.unb.br> Co-authored-by: Pedro Igor <pedroigor.silva@outlook.com>

---
+++ src/models/Conteudo/index.js
@@ -0,0 +1,33 @@
+import ConteudoModel from "./Conteudo";
+
+exports.getAll = async () => {
+  return await ConteudoModel.findAll({
+    raw: true,
+  });
+};
+exports.searchByName = async (nome) => {
+  return await ConteudoModel.findOne({
+    where: {
+      nome: nome,
+    },
+  });
+};
+exports.create = async () => {
+  return await ConteudoModel.create({});
+};
+exports.searchById = async (id) => {
+  return await ConteudoModel.findByPk(id);
+};
+exports.searchAll = async () => {
+  return await ConteudoModel.findAll();
+};
+exports.delete = async (id) => {
+  return await ConteudoModel.destroy({
+    where: {
+      id_conteudo: id,
+    },
+  });
+};
+exports.editById = async ({ status }, id) => {
+  return await ConteudoModel.update({ status }, { where: { id_conteudo: id } });
+};
