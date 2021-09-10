7830f4c95406f21c1701f2314a1059bc6f0ac717
CarlosZoft <carlosoftwar@gmail.com>
2021-09-09 23:42:27 -0300

    update: alterando delete de etnia, para dele conteudo
    Co-authored-by: Herick <herick.lima77@gmail.com>
    Co-authored-by: Helder <180121847@aluno.unb.br>
    Co-authored-by: Pedro Igor <pedroigor.silva@outlook.com>

--- src/controllers/Etnia/deleteEtnia.js
+++ src/controllers/Etnia/deleteEtnia.js
@@ -1,5 +1,6 @@
 import Etnia from "../../models/Etnia";
 import { HttpException } from "../../error/HttpException";
+import Conteudo from "../../models/Conteudo";
 
 exports.deleteOne = async (request, response) => {
   const { id_etnia } = request.params;
@@ -7,10 +8,11 @@ exports.deleteOne = async (request, response) => {
     throw new HttpException(400, "ID inválido - Etnia");
   }
 
-  const etniaEncontrada = await Etnia.delete(id_etnia);
+  const etniaEncontrada = await Etnia.searchById(id_etnia);
   if (!etniaEncontrada) {
     throw new HttpException(404, "Etnia não encontrada - Etnia");
   }
+  Conteudo.delete(etniaEncontrada.id_conteudo);
 
   response.send("Deletado com sucesso!");
 };
