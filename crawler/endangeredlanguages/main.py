ec65afb73491477c5ada44223f0de7f3a89443df
Pedro Igor <pedroigor.silva@outlook.com>
2021-09-29 19:53:42 -0300

    feat: update data crawler and get language position

--- crawler/endangeredlanguages/main.py
+++ crawler/endangeredlanguages/main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import re
+import math
 df = pd.read_csv('database_file.csv')
 for value in df.values:
     if(re.match(r".*Brazil", str(value[10]))):
         lingua = value[2]
-        povos = re.sub(r'(.*)\s?(;)\s?(.*)', r'\1\2\3', str(value[3])).replace('\r', '').replace('\n', '').split(';')
-        
-        print(povos)
+        etnias = re.sub(r'(.*)\s?(;)\s?(.*);\s?', r'\1\2\3', str(value[3])).replace('\r', '').replace('\n', '').replace('; ', ';').split(';')
+        if(str(value[12])!='nan'):
+            if(';' in value[12]):
+                posicao = str(value[12]).replace(' ', '').split(';')[0].split(',')
+            else:
+                posicao = str(value[12]).replace(' ', '').split(',')
