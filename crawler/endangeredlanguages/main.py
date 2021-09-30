0f18e8b2ffbfab102c46145e31fd39e2f20b56f9
Pedro Igor <pedroigor.silva@outlook.com>
2021-09-30 19:44:59 -0300

    feat: improve data show and add output

--- crawler/endangeredlanguages/main.py
+++ crawler/endangeredlanguages/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import re
-import math
 df = pd.read_csv('database_file.csv')
 for value in df.values:
     if(re.match(r".*Brazil", str(value[10]))):
         lingua = value[2]
-        etnias = re.sub(r'(.*)\s?(;)\s?(.*);\s?', r'\1\2\3', str(value[3])).replace('\r', '').replace('\n', '').replace('; ', ';').split(';')
+        outros_nomes = re.sub(r'(.*)\s?(;)\s?(.*);\s?', r'\1\2\3', str(value[3])).replace('\r', '').replace('\n', '').replace('; ', ';').split(';')
+        print('LINGUA: '+lingua)
+        print('TAMBÉM CONHECIDA COMO: '+', '.join(outros_nomes))
+        posicao = None
         if(str(value[12])!='nan'):
             if(';' in value[12]):
                 posicao = str(value[12]).replace(' ', '').split(';')[0].split(',')
             else:
                 posicao = str(value[12]).replace(' ', '').split(',')
+        if(posicao):
+            print('POSICAO GEOGRAFICA: '+' '.join(posicao))
+        else:
+            print('POSICAO GEOGRAFICA INVALIDA OU INDISPONIVEL')
