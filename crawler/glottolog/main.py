fb1593bdef7c9dd0baa15c6dd0fd4051ceed205c
Pedro Igor <pedroigor.silva@outlook.com>
2021-10-05 16:30:41 -0300

    feat: add relantion between language and family

--- crawler/glottolog/main.py
+++ crawler/glottolog/main.py
@@ -1,8 +1,20 @@
 import json
-json_file = open('geojson.json')
-geojson = json.loads(json_file.read())
-for feature in geojson['features']:
-    lingua = feature['properties']['name']
-    posicao = feature['geometry']['coordinates']
-    print('LINGUA: '+lingua)
-    print('POSICAO GEOGRAFICA: '+str(posicao[0])+' '+str(posicao[1]))
+language_json = open('language.json')
+family_json = open('family.json')
+languages = json.loads(language_json.read())['features']
+families = json.loads(family_json.read())['features']
+
+def get_family(families, family_pk):
+    if(family_pk!='null'):
+        for family in families:
+            family_info = family['properties']['language']
+            if(int(family_info['pk']) == int(family_pk)):
+                return family_info
+
+for language in languages:
+    language_info = language['properties']['language']
+    language_name = language_info['name']
+    language_latitude = language_info['latitude']
+    language_longitude = language_info['longitude']
+    language_family_pk = language_info['family_pk']
+    print(get_family(families, language_family_pk))
