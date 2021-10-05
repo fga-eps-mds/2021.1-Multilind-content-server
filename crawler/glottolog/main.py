f328105710258c48e631ea78eeeb4c2a4d3b321d
Pedro Igor <pedroigor.silva@outlook.com>
2021-10-05 20:46:55 -0300

    feat: use language initials to find family

--- crawler/glottolog/main.py
+++ crawler/glottolog/main.py
@@ -5,16 +5,24 @@ languages = json.loads(language_json.read())['features']
 families = json.loads(family_json.read())['features']
 
 def get_family(families, family_pk):
-    if(family_pk!='null'):
+    if(family_pk!='null' and family_pk):
         for family in families:
             family_info = family['properties']['language']
             if(int(family_info['pk']) == int(family_pk)):
+                print(family_info)
                 return family_info
-
+def get_family_by_newick(families, id):
+    for family in families:
+        family_info = family['properties']['language']
+        print(family_info['newick'])
+        if(f"[{str(id)}]" in family_info['newick']):
+            print(family_info)
+            return family_info
 for language in languages:
     language_info = language['properties']['language']
     language_name = language_info['name']
     language_latitude = language_info['latitude']
     language_longitude = language_info['longitude']
     language_family_pk = language_info['family_pk']
-    print(get_family(families, language_family_pk))
+    if(language_family_pk):
+        get_family_by_newick(families, language_family_pk)
