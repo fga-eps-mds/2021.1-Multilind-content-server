a91322eb5f6a9235dde5a2d812c694460d9acfff
Pedro Igor <pedroigor.silva@outlook.com>
2021-10-06 18:52:50 -0300

    feat: add dialeto migration

---
+++ src/database/migrations/20211006214829-createTable-Dialeto.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable("Dialeto", {
+        id_etnia: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Etnia",
+            key: "id_etnia",
+          },
+          onUpdate: "RESTRICT",
+          onDelete: "RESTRICT",
+        },
+        id_lingua: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Lingua",
+            key: "id_lingua",
+          },
+          onUpdate: "RESTRICT",
+          onDelete: "RESTRICT",
+        },
+        id_conteudo: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Conteudo",
+            key: "id_conteudo",
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        }
+      });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("Dialeto");
+  },
+};
