bc99713820c00351e47f0b7b6d0be59a90e11e74
Pedro Igor <pedroigor.silva@outlook.com>
2021-10-05 22:23:22 -0300

    feat: use requests to get Brasil languages tronco

--- crawler/glottolog/main.py
+++ crawler/glottolog/main.py
@@ -1,28 +1,25 @@
 import json
-language_json = open('language.json')
-family_json = open('family.json')
+import requests
+language_json = open('brasil.json')
 languages = json.loads(language_json.read())['features']
-families = json.loads(family_json.read())['features']
-
-def get_family(families, family_pk):
-    if(family_pk!='null' and family_pk):
-        for family in families:
-            family_info = family['properties']['language']
-            if(int(family_info['pk']) == int(family_pk)):
-                print(family_info)
-                return family_info
-def get_family_by_newick(families, id):
-    for family in families:
-        family_info = family['properties']['language']
-        print(family_info['newick'])
-        if(f"[{str(id)}]" in family_info['newick']):
-            print(family_info)
-            return family_info
+print(len(languages))
 for language in languages:
     language_info = language['properties']['language']
     language_name = language_info['name']
     language_latitude = language_info['latitude']
     language_longitude = language_info['longitude']
     language_family_pk = language_info['family_pk']
+    language_id = language_info['id']
+    print(f'Língua: {language_name}')
+    print(f'Posição Geográfica: {language_latitude} {language_longitude}')
     if(language_family_pk):
-        get_family_by_newick(families, language_family_pk)
+        language_details_url = f"https://glottolog.org/resource/languoid/id/{language_id}.json"
+        language_response = requests.get(language_details_url)
+        language_details = language_response.json()
+        classifications = language_details['classification']
+        for classification in classifications:
+            family_url = f"https://glottolog.org/resource/languoid/id/{classification['id']}.json"
+            family_response = requests.get(family_url)
+            family_details = family_response.json()
+            if(family_details['pk'] == language_family_pk):
+                print(f"Tronco: {family_details['name']}")
