744db684c9ce391884d6a056d8112e94a3639a0c
CarlosZoft <carlosoftwar@gmail.com>
2021-10-09 22:58:02 -0300

    fix: code smells
    Co-authored-by: Herick <herick.lima77@gmail.com>

--- src/models/Conteudo/index.js
+++ src/models/Conteudo/index.js
@@ -10,24 +10,3 @@ exports.delete = async (id) => {
     },
   });
 };
-// exports.getAll = async () => {
-//   return ConteudoModel.findAll({
-//     raw: true,
-//   });
-// };
-// exports.searchByName = async (nome) => {
-//   return ConteudoModel.findOne({
-//     where: {
-//       nome: nome,
-//     },
-//   });
-// };
-// exports.searchById = async (id) => {
-//   return ConteudoModel.findByPk(id);
-// };
-// exports.searchAll = async () => {
-//   return ConteudoModel.findAll();
-// };
-// exports.editById = async ({ status }, id) => {
-//   return ConteudoModel.update({ status }, { where: { id_conteudo: id } });
-// };
