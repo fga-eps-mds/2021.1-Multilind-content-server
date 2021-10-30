f730f70a20e7b574179d52179dfd0c3398f60cef
CarlosZoft <carlosoftwar@gmail.com>
2021-10-30 01:08:27 -0300

    test: making tests to created methods
    Co-authored-by: Helder <180121847@aluno.unb.br>

--- src/__tests__/units/Localidade/index.js
+++ src/__tests__/units/Localidade/index.js
@@ -7,17 +7,50 @@ describe("\n## TESTES LOCALIDADE\n", () => {
         latitude: 123.5,
         longitude: 456.9,
       });
-      expect(localidade.dataValues).toEqual({
+      expect(localidade).toMatchObject({
         id_localidade: 1,
         latitude: 123.5,
         longitude: 456.9,
       });
     });
+    it("Criando localidade com o metodo createBulkInsert()", async () => {
+      const localidade = await modelLocalidade.createBulkInsert([
+        {
+          latitude: 123.53,
+          longitude: 456.934,
+        },
+        {
+          latitude: 123.534,
+          longitude: 456.9344,
+        },
+        {
+          latitude: 123.531,
+          longitude: 456.9345,
+        }
+      ]);
+      expect(localidade).toMatchObject([
+        {
+          id_localidade: 2,
+          latitude: 123.53,
+          longitude: 456.934,
+        },
+        {
+          id_localidade: 3,
+          latitude: 123.534,
+          longitude: 456.9344,
+        },
+        {
+          id_localidade: 4,
+          latitude: 123.531,
+          longitude: 456.9345,
+        }
+      ]);
+    });
   });
   describe("Consulta de localidade", () => {
     it("Consultando com metodo searchByID(1)", async () => {
       const localidade = await modelLocalidade.searchById(1);
-
+  
       expect(localidade).toMatchObject({
         id_localidade: 1,
         latitude: 123.5,
@@ -26,14 +59,30 @@ describe("\n## TESTES LOCALIDADE\n", () => {
     });
     it("Consultando com metodo getAll()", async () => {
       const localidade = await modelLocalidade.getAll();
-
+  
       expect(localidade).toMatchObject([
         {
           id_localidade: 1,
           latitude: 123.5,
           longitude: 456.9,
         },
+        {
+          id_localidade: 2,
+          latitude: 123.53,
+          longitude: 456.934,
+        },
+        {
+          id_localidade: 3,
+          latitude: 123.534,
+          longitude: 456.9344,
+        },
+        {
+          id_localidade: 4,
+          latitude: 123.531,
+          longitude: 456.9345,
+        }
       ]);
     });
   });
+  
 });
